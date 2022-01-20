7b5b9c28bb96042e640acaa6da1610e98c99f9e5
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-01-21 00:31:30 +0100

    Fix printf in test.c

--- test.c
+++ test.c
@@ -4,8 +4,8 @@ int main() {
   uint32_t p = P1_UInt32_main ();
   printf("r=%ui\n", p);
   uint64_t h = P1_UInt32_main2 ();
-  printf("h=%ui\n", h);
+  printf("h=%lui\n", h);
   uint64_t s = P1_UInt32_main3 ();
-  printf("s=%ui\n", s);
+  printf("s=%lui\n", s);
   return 0;
 }
