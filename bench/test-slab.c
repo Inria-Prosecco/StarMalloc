5a45acec849d5904f9043bcd6751efd64727271b
Antonin Reitz <antonin.reitz@inria.fr>
2022-12-16 16:07:25 +0100

    improve slab test
    test several size classes
    PS: build tested with
    F*: nik_size_t
    krml: master

--- bench/test-slab.c
+++ bench/test-slab.c
@@ -6,9 +6,12 @@ void* slab_malloc(size_t size);
 int main(){
   uint8_t* ptr = NULL;
   uint32_t bound = 256ul;
-  uint32_t size_class = 64ul;
   for (uint32_t i = 0; i < 256; ++i) {
-    ptr = slab_malloc(size_class);
+    ptr = slab_malloc(12);
+    ptr[1] = 1;
+    ptr = slab_malloc(37);
+    ptr[1] = 1;
+    ptr = slab_malloc(29);
     ptr[1] = 1;
   }
   return 0;
