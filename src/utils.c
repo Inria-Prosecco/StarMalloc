24c62f8911e71670082dc69a1489074af41b492b
Antonin Reitz <antonin.reitz@inria.fr>
2023-01-26 03:38:20 +0100

    size-classes: fix ffs64 wrapper

--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
-//#include <stdio.h>
-//
 #include "Slabs.h"
 
 uint32_t Utils2_ffs64(uint64_t x) {
-  return __builtin_ffsll(~x);
+  //return __builtin_ffsll(~x) - 1;
+  return __builtin_ctzll(~x);
 }
 
 ptrdiff_t Utils2_ptrdiff(void* arr1, void* arr2) {
   ptrdiff_t r = arr1 - arr2;
   return r;
 }
-
-//int main(){
-//  uint64_t v;
-//  v = 1UL;
-//  v = 4UL;
-//  //v = 2UL;
-//  uint32_t r = ffs64(v);
-//  printf("%lu", r);
-//  return 0;
-//}
