cc06fe7fc6b76b8b133e0e15224c2bb6c220bfce
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2023-02-15 15:26:11 +0100

    cleanup handwritten C utils

--- src/utils.c
+++ src/utils.c
@@ -1,13 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
-#include "Slabs.h"
 
 uint32_t Utils2_ffs64(uint64_t x) {
   //return __builtin_ffsll(~x) - 1;
   return __builtin_ctzll(~x);
-}
-
-ptrdiff_t Utils2_ptrdiff(void* arr1, void* arr2) {
-  ptrdiff_t r = arr1 - arr2;
-  return r;
 }
