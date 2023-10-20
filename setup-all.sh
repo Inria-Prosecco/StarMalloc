baeb4d90cd0342d203cffd0724d765590922075b
Antonin Reitz <antonin.reitz@inria.fr>
2023-10-20 21:15:02 +0200

    setup-all.sh: add vm.max_map_count check

--- setup-all.sh
+++ setup-all.sh
@@ -47,6 +47,12 @@ setup() {
       #last line = missing for tcg/pa, with bazel-bootstrap
     echo "Required Debian packages are installed"
   fi
+
+  if [[ "$(sysctl -n vm.max_map_count)" -lt 1048576 ]]; then
+	  echo "sysctl vm.max_map_count < 1048576, not OK, please fix it, exiting"
+  else
+	  echo "sysctl vm.max_map_count >= 1048576, OK"
+  fi
 }
 
 build_starmalloc() {
