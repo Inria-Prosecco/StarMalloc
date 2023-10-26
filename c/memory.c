39aa639fdc82fc44ca27ffff2cbe39e90d7b0d94
Antonin Reitz <antonin.reitz@inria.fr>
2023-10-26 15:25:05 +0200

    syscall wrappers: errno checks + fatal_error

--- c/memory.c
+++ c/memory.c
@@ -2,25 +2,35 @@
 
 #include <errno.h>
 #include <sys/mman.h>
-#include <assert.h>
 
 #include "internal/StarMalloc.h"
 #include "fatal_error.h"
 
+// syscall wrapper: initialization (fatal error on failure)
 uint8_t *mmap_init(size_t size) {
   void* ptr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_NORESERVE, -1, 0);
-  assert (ptr != NULL);
+  if (ptr == MAP_FAILED) {
+    if (errno != ENOMEM) {
+      fatal_error("non-ENOMEM mmap failure");
+    }
+    fatal_error ("mmap failed during initialization (returned NULL)");
+  }
   return ptr;
 }
 
+// syscall wrapper (large allocator allocation wrapper, can return NULL)
 uint8_t *mmap_noinit(size_t size) {
-  return mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+  void* ptr = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_NORESERVE, -1, 0);
+  if (ptr == MAP_FAILED) {
+    if (errno != ENOMEM) {
+      fatal_error("non-ENOMEM mmap failure");
+    }
+    return NULL;
+  }
+  return ptr;
 }
 
-uint8_t *mmap_u8(size_t len) {
-  return (uint8_t*) mmap_noinit(len * sizeof(uint8_t));
-}
-
+// syscall wrapper (large allocator deallocation wrapper)
 void munmap_u8(uint8_t* ptr, size_t len) {
   bool b = munmap((void*) ptr, len);
   if (b && errno != ENOMEM) {
@@ -29,52 +39,62 @@ void munmap_u8(uint8_t* ptr, size_t len) {
   return;
 }
 
+// syscall wrapper
+void mmap_strict_trap (uint8_t* ptr, size_t len) {
+  void* p = mmap((void*) ptr, len, PROT_NONE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_FIXED, -1, 0);
+  if (p == MAP_FAILED && errno != ENOMEM) {
+    fatal_error("non-ENOMEM mmap failure");
+  }
+  return;
+}
+
+// syscall wrapper
+void mmap_trap (uint8_t* ptr, size_t len) {
+  int r = madvise((void*) ptr, len, MADV_DONTNEED);
+  if (r && errno != ENOMEM) {
+    fatal_error("non-ENOMEM MADV_DONTNEED madvise failure");
+  }
+  return;
+}
+
+// monomorphised functions
+
+// large allocator allocation wrapper
+uint8_t *mmap_u8(size_t len) {
+  return (uint8_t*) mmap_noinit(len * sizeof(uint8_t));
+}
+
+// slabs allocator init: initial large slabs mapping
 uint8_t *mmap_u8_init(size_t len) {
   return (uint8_t*) mmap_init(len * sizeof(uint8_t));
 }
 
+// slabs allocator init: initial large slabs mapping
 uint64_t *mmap_u64_init(size_t len) {
   return (uint64_t*) mmap_init(len * sizeof(uint64_t));
 }
 
+// slabs allocator init: initial large slabs mapping
 ArrayList_cell *mmap_cell_status_init(size_t len) {
   return (ArrayList_cell*) mmap_init(len * sizeof(ArrayList_cell));
 }
 
-//TODO: to be removed
-//uint32_t *mmap_ptr_u32() {
-//  return (uint32_t*) mmap_init(sizeof(uint32_t));
-//}
-
+// slabs allocator init
 size_t *mmap_ptr_us_init() {
   return (size_t*) mmap_init(sizeof(size_t));
 }
 
+// slabs allocator init
 size_class* mmap_sc_init(size_t len) {
   return (size_class*) mmap_init(len * sizeof(size_class));
 }
 
+// slabs allocator init
 uint32_t* mmap_sizes_init (size_t len) {
   return (uint32_t*) mmap_init(len * sizeof(uint32_t));
 }
 
-void mmap_strict_trap (uint8_t* ptr, size_t len) {
-  void* p = mmap((void*) ptr, len, PROT_NONE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_FIXED, -1, 0);
-  if (p == MAP_FAILED && errno != ENOMEM) {
-    assert (false);
-  }
-  return;
-}
-
-void mmap_trap (uint8_t* ptr, size_t len) {
-  int r = madvise((void*) ptr, len, MADV_DONTNEED);
-  if (r && errno != ENOMEM) {
-    assert (false);
-  }
-  return;
-}
-
-
+// large allocator init
 Impl_Trees_Types_node** mmap_ptr_metadata() {
   return (Impl_Trees_Types_node**) mmap_init(sizeof(Impl_Trees_Types_node*));
 }
