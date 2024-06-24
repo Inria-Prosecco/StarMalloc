37db6e831d195a35012278a111f6d9ea29fc2925
Antonin Reitz <antonin.reitz@inria.fr>
2024-06-24 20:30:07 +0200

    dist/: rm unused code, inline ArrayList code

--- dist/Utils2.h
+++ dist/Utils2.h
@@ -6,13 +6,9 @@
 
 #include "krmllib.h"
 
-typedef void *Utils2_same_base_array;
-
 uint32_t Utils2_nb_slots(uint32_t size_class);
 
 size_t Utils2_rounding(uint32_t size_class);
-
-typedef void *Utils2_zf_b;
 
 uint64_t Utils2_full_n_aux(uint32_t bound);
 
@@ -25,10 +21,6 @@ bool Utils2_is_empty_s(uint32_t size_class, uint64_t *md);
 bool Utils2_is_partial_s(uint32_t size_class, uint64_t *md);
 
 bool Utils2_is_full_s(uint32_t size_class, uint64_t *md);
-
-typedef void *Utils2_zf_u64;
-
-typedef void *Utils2_zf_u8;
 
 
 #define __Utils2_H_DEFINED
