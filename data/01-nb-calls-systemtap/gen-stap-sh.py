25d538d25e80ebb625a120805455dbfcb44d69f2
Antonin Reitz <antonin.reitz@inria.fr>
2025-02-18 16:11:20 +0100

    data/: mv SystemTap script to corresponding dir
    + add short README

--- data/01-nb-calls-systemtap/gen-stap-sh.py
+++ data/01-nb-calls-systemtap/gen-stap-sh.py
@@ -1,8 +1,11 @@
+# working on Arch Linux, not on NixOS
+# requires a glibc with debug symbols
+
 L1 = ["barnes", "cfrac", "espresso", "gs", "lean", "redis", "larson", "larson-sized", "lua", "z3"]
 L2 = ["alloc-test", "cscratch", "cthrash", "glibc-simple", "glibc-thread", "lean-mathlib", "malloc-large", "mleak", "mstress", "rbstress", "rocksdb", "rptest", "sh6bench", "sh8bench", "xmalloc-test"]
 L = L1+L2
 
-dir = "hists2"
+dir = "hists"
 
 for e in L:
   print("time stap log.stp -c \"bash ../../bench.sh sys "+e+"\" &> "+dir+"/"+e+".txt")
