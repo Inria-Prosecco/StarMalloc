dca1e096e7bf21ac753bbc05a07a727c9d25ac3f
Antonin Reitz <antonin.reitz@inria.fr>
2025-04-01 16:52:34 +0200

    IPT traces: refine hwlogmalloc.c
    - some guidelines to add other allocation/deallocation primitives
    - execution time of allocation/deallocation primitives for free (when using log_ipt carefully)

--- data/03-full-allocation-calls-ipt/hwlogmalloc.c
+++ data/03-full-allocation-calls-ipt/hwlogmalloc.c
@@ -26,6 +26,15 @@ static inline bool is_init(void) {
   return b;
 }
 
+// invariants to be preserved when adding other functions (e.g. calloc, realloc, etc)
+// 1. before each allocation/deallocation primitive call, log_ipt(magic_value)
+// 2. this magic value must be only used by the considered allocation/deallocation primitive
+// 3. there must be one log_ipt call right after the considered allocation/deallocation primitive
+
+// considered data:
+// - full allocation/deallocation graph, with data on address reuse + allocation/deallocation arguments
+// - execution time of each allocation/deallocation primitive depending on the arguments (e.g. the requested size)
+
 static void init(void) {
   static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_lock(&m);
@@ -35,16 +44,9 @@ static void init(void) {
   }
   real_malloc = dlsym(RTLD_NEXT, "malloc");
   real_free = dlsym(RTLD_NEXT, "free");
-  //log_ipt((uint64_t) real_malloc);
-  //log_ipt((uint64_t) real_free);
-  uint64_t x = -1;
-  //log_ipt(x);
 
   atomic_store_explicit(&check_init, true, memory_order_release);
   pthread_mutex_unlock(&m);
-  //if (NULL == real_malloc) {
-  //  fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-  //}
 }
 
 
@@ -52,11 +54,11 @@ void* malloc(size_t size) {
   if(! is_init()) {
     init();
   }
-
-  void *p = NULL;
+  // execution start
   log_ipt((uint64_t) (-1L));
   log_ipt((uint64_t) size);
-  p = real_malloc(size);
+  void* p = real_malloc(size);
+  // execution end
   log_ipt((uint64_t) p);
   return p;
 }
@@ -65,8 +67,11 @@ void free(void* ptr) {
   if(! is_init()) {
     init();
   }
-  uint64_t ptr_as_u64 = (uint64_t) ((uintptr_t) ptr);
+  // execution start
   log_ipt((uint64_t) (-2L));
-  log_ipt(ptr_as_u64);
+  uint64_t ptr_as_u64 = (uint64_t) ((uintptr_t) ptr);
   real_free(ptr);
+  // execution end
+  log_ipt(ptr_as_u64);
 }
+
